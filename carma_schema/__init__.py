bb49fab3fc4e4f23706094851b2c1a0d8dcda78a
Brian Miles <selimnairb@gmail.com>
2020-07-29 22:12:12 -0500

    CN-34: Update validator to report multiple errors for base schema validation; Update schema to constrain numeric values; Add additional validation tests

--- carma_schema/__init__.py
+++ carma_schema/__init__.py
@@ -22,31 +22,32 @@ def validate(schema_path: str, document_path: str) -> (bool, dict):
     with open(document_path, 'r') as f:
         document = json.load(f)
 
-    try:
-        jsonschema.validate(document, schema)
-        # Create an error for higher-order validation errors after jsonschema validation
-        errors = []
+    errors = []
+    validator = jsonschema.Draft7Validator(schema)
+    for e in validator.iter_errors(document):
+        errors.append(e)
 
-        # Get set of HUC-12 defined in HUC12Watersheds (jsonschema and CARMA schema will make sure HUC12s are unique)
-        huc12_ids = {h['id'] for h in document['HUC12Watersheds']}
+    # Basic validation against schema failed, bail out before trying higher-order validation
+    # (i.e. validating that HUC12 references are correct)
+    if len(errors) > 0:
+        return False, {'errors': errors}
 
-        # Make sure that HUC-12s references from: GroundWaterAvailabilityDatasets,
-        # SurfaceWaterAvailabilityDatasets, WaterUseDatasets
-        # GroundWaterAvailabilityDatasets are actually defined.
-        for dataset_type in DATASET_TYPES:
-            undef_huc12 = set()
-            dataset_huc12s = [d['huc12'] for d in document[dataset_type]]
-            for huc in dataset_huc12s:
-                if huc not in huc12_ids:
-                    undef_huc12.add(huc)
-            if len(undef_huc12):
-                errors.append(f"Undefined HUC12s encountered in {dataset_type}: {undef_huc12}")
+    # Get set of HUC-12 defined in HUC12Watersheds (jsonschema and CARMA schema will make sure HUC12s are unique)
+    huc12_ids = {h['id'] for h in document['HUC12Watersheds']}
 
-        if len(errors) > 0:
-            return False, {'errors': errors}
-        else:
-            return True, {}
-    except jsonschema.exceptions.SchemaError as e:
-        return False, {"errors": [e]}
-    except jsonschema.exceptions.ValidationError as e:
-        return False, {"errors": [e]}
+    # Make sure that HUC-12s references from: GroundWaterAvailabilityDatasets,
+    # SurfaceWaterAvailabilityDatasets, WaterUseDatasets
+    # GroundWaterAvailabilityDatasets are actually defined.
+    for dataset_type in DATASET_TYPES:
+        undef_huc12 = set()
+        dataset_huc12s = [d['huc12'] for d in document[dataset_type]]
+        for huc in dataset_huc12s:
+            if huc not in huc12_ids:
+                undef_huc12.add(huc)
+        if len(undef_huc12):
+            errors.append(f"Undefined HUC12s encountered in {dataset_type}: {undef_huc12}")
+
+    if len(errors) > 0:
+        return False, {'errors': errors}
+    else:
+        return True, {}
