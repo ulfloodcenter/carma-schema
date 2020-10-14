65b5ec3085383cecec8352abb220e898bc22da8a
Brian Miles <selimnairb@gmail.com>
2020-10-13 22:17:50 -0500

    CN-16: Begin of work to allow water use data sets to be associated with a county or a HUC-12

--- carma_schema/__init__.py
+++ carma_schema/__init__.py
@@ -35,17 +35,23 @@ def validate(schema_path: str, document_path: str) -> (bool, dict):
     # Get set of HUC-12 defined in HUC12Watersheds (jsonschema and CARMA schema will make sure HUC12s are unique)
     huc12_ids = {h['id'] for h in document['HUC12Watersheds']}
 
-    # Make sure that HUC-12s references from: GroundWaterAvailabilityDatasets,
+    # Make sure that HUC-12s and county ID references from: GroundWaterAvailabilityDatasets,
     # SurfaceWaterAvailabilityDatasets, WaterUseDatasets
     # GroundWaterAvailabilityDatasets are actually defined.
     for dataset_type in DATASET_TYPES:
+        # Validate HUC-12 IDs
         undef_huc12 = set()
-        dataset_huc12s = [d['huc12'] for d in document[dataset_type]]
-        for huc in dataset_huc12s:
-            if huc not in huc12_ids:
-                undef_huc12.add(huc)
+        try:
+            dataset_huc12s = [d['huc12'] for d in document[dataset_type]]
+            for huc in dataset_huc12s:
+                if huc not in huc12_ids:
+                    undef_huc12.add(huc)
+        except KeyError:
+            pass
         if len(undef_huc12):
             errors.append(f"Undefined HUC12s encountered in {dataset_type}: {undef_huc12}")
+        # Validate county IDs
+        # TODO
 
     if len(errors) > 0:
         return False, {'errors': errors}
