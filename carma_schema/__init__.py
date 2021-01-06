05a497c48498cee1537169faf729b3ee1d65826f
Brian Miles <selimnairb@gmail.com>
2021-01-06 13:27:11 -0500

    CN-43: Update validator to: (1) ensure one of HUC12Watersheds or Counties definitions must be present in a document; and (2) validate county IDs in sub datasets

--- carma_schema/__init__.py
+++ carma_schema/__init__.py
@@ -33,7 +33,23 @@ def validate(schema_path: str, document_path: str) -> (bool, dict):
         return False, {'errors': errors}
 
     # Get set of HUC-12 defined in HUC12Watersheds (jsonschema and CARMA schema will make sure HUC12s are unique)
-    huc12_ids = {h['id'] for h in document['HUC12Watersheds']}
+    if 'HUC12Watersheds' not in document:
+        huc12_present = False
+        huc12_ids = set()
+    else:
+        huc12_present = True
+        huc12_ids = {h['id'] for h in document['HUC12Watersheds']}
+
+    if 'Counties' not in document:
+        counties_present = False
+        county_ids = set()
+    else:
+        counties_present = True
+        county_ids = {h['id'] for h in document['Counties']}
+
+    if not (huc12_present or counties_present):
+        errors.append(f"Neither HUC12Watersheds nor Counties encountered in document {document_path}, "
+                      "when at least one must be present")
 
     # Make sure that HUC-12s and county ID references from: GroundWaterAvailabilityDatasets,
     # SurfaceWaterAvailabilityDatasets, WaterUseDatasets
@@ -51,7 +67,16 @@ def validate(schema_path: str, document_path: str) -> (bool, dict):
         if len(undef_huc12):
             errors.append(f"Undefined HUC12s encountered in {dataset_type}: {undef_huc12}")
         # Validate county IDs
-        # TODO
+        undef_county = set()
+        try:
+            dataset_counties = [d['county'] for d in document[dataset_type]]
+            for county in dataset_counties:
+                if county not in county_ids:
+                    undef_county.add(county)
+        except KeyError:
+            pass
+        if len(undef_county):
+            errors.append(f"Undefined counties encountered in {dataset_type}: {undef_county}")
 
     if len(errors) > 0:
         return False, {'errors': errors}
